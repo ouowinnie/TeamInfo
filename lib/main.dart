ddf1513166baa17a4e3348ba165b45f62836f228
DESKTOP-ED8VI87\User <dydrkfl078@naver.com>
2023-07-11 19:42:48 +0900

    오류

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:team_page/Jina_personal.dart';
-import 'package:team_page/choonghwan_personal.dart';
-import 'package:team_page/daeul_personal.dart';
-import 'package:team_page/sohyun_personal.dart';
-import 'package:team_page/visitor_memo_list.dart';
-import 'package:team_page/yongjun_personal.dart';
-=======
 import 'package:provider/provider.dart';
 import 'package:team_page/visitor_memo_list.dart';
 
 import 'guestbook_service.dart';
 import 'team_page.dart';
->>>>>>> 953f1706c97b9f2058700677278027f6d53d01c1
 
 void main() {
   runApp(
@@ -32,82 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home: VisitorMemoList(),
-<<<<<<< HEAD
-    );
-  }
-}
-
-class TeamPage extends StatelessWidget {
-  const TeamPage({super.key}); // 생성자
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("팀 프로젝트"),
-      ),
-      body: Column(
-        children: [
-          Text("팀 페이지 초안"),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => ChoongHwanPersonal(),
-                  ),
-                );
-              },
-              child: Text("이충환")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => DaeulPersonal(),
-                  ),
-                );
-              },
-              child: Text("이다을")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => JinAPersonal(),
-                  ),
-                );
-              },
-              child: Text("김진아")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => YongjunPersonal(),
-                  ),
-                );
-              },
-              child: Text("이용준")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => SohyunPersonal(),
-                  ),
-                );
-              },
-              child: Text("김소현")),
-        ],
-      ),
-=======
->>>>>>> 953f1706c97b9f2058700677278027f6d53d01c1
-=======
       home: TeamPage(),
->>>>>>> e3fd2281145c89d5d114e7020fc8b51338d810de
     );
   }
 }
--- lib/visitor_memo_list.dart
+++ lib/visitor_memo_list.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:team_page/guestbook.dart';
-import 'package:team_page/guestbook_service.dart';
-=======
 import 'package:provider/provider.dart';
 import 'package:team_page/guestbook_service.dart';
 
 import 'guestbook.dart';
->>>>>>> 953f1706c97b9f2058700677278027f6d53d01c1
 
 class VisitorMemoList extends StatefulWidget {
   const VisitorMemoList({super.key});
@@ -17,37 +12,6 @@ class VisitorMemoList extends StatefulWidget {
   State<VisitorMemoList> createState() => _VisitorMemoListState();
 }
 
-<<<<<<< HEAD
-class _HomePageState extends State<VisitorMemoList> {
-  List<String> visitList = ["테스트11", "테스트"]; // 전체 메모 목록
-
-  @override
-  Widget build(BuildContext context) {
-    //   return Consumer<GuestbookService>(
-    //     builder: (context, guestbookService, child) {
-    // List<Guestbook> visitList = GuestbookService.visitList
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("방명록"),
-        backgroundColor: Colors.grey,
-      ),
-      body: visitList.isEmpty
-          ? Center(child: Text("방명록을 작성해 주세요"))
-          : ListView.builder(
-              itemCount: visitList.length,
-              itemBuilder: (context, index) {
-                String guestbook = visitList[index]; //String -> Guestbook
-                return Container(
-                  width: 400,
-                  height: 70,
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      ListTile(
-                        title: Text(
-                          guestbook,
-                          maxLines: 3,
-=======
 class _VisitorMemoListState extends State<VisitorMemoList> {
   @override
   Widget build(BuildContext context) {
@@ -70,58 +34,19 @@ class _VisitorMemoListState extends State<VisitorMemoList> {
                         title: Text(
                           memo.content,
                           maxLines: 1,
->>>>>>> 953f1706c97b9f2058700677278027f6d53d01c1
                           overflow: TextOverflow.ellipsis,
                         ),
                         onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-<<<<<<< HEAD
-                              builder: (_) => createMemoPage(
-                                index: index,
-                                visitList: visitList,
-=======
                               builder: (_) => CreateMemoPage(
                                 index: index,
->>>>>>> 953f1706c97b9f2058700677278027f6d53d01c1
                               ),
                             ),
                           );
                         },
                       ),
-<<<<<<< HEAD
-                      Container(
-                        height: 1,
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // guestbook.creatMemoTitle(content: '');
-          String guestbook = '';
-          setState(() {
-            visitList.add(guestbook);
-          });
-
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (_) => createMemoPage(
-                      index: visitList.indexOf(guestbook),
-                      visitList: visitList,
-                    )),
-          );
-        },
-        label: Text("방명록 쓰기"),
-        icon: Icon(Icons.add),
-      ),
-    );
-=======
                       Container(height: 1, color: Colors.black)
                     ],
                   );
@@ -143,6 +68,5 @@ class _VisitorMemoListState extends State<VisitorMemoList> {
         ),
       );
     });
->>>>>>> 953f1706c97b9f2058700677278027f6d53d01c1
   }
 }
